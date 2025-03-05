7416b73fb20a619bbd2d08fe9ac33bcc5abff973
Fabiola Contreras <fcontrerascol@gmail.com>
2025-03-04 20:34:22 -0600

    feat: implement digit classification model

--- DigitClassification.py
+++ DigitClassification.py
@@ -2,12 +2,6 @@ from torch import no_grad, stack
 from torch.utils.data import DataLoader
 from torch.nn import Module
 
-
-"""
-Functions you should use.
-Please avoid importing any other functions or modules.
-Your code will not pass if the gradescope autograder detects any changed imports
-"""
 import torch
 from torch.nn import Parameter, Linear
 from torch import optim, tensor, tensordot, ones, matmul
@@ -17,194 +11,107 @@ from torch import movedim
 
 class DigitClassificationModel(Module):
     """
-    A model for handwritten digit classification using the MNIST dataset.
+    Modelo para la clasificación de dígitos escritos a mano utilizando el conjunto de datos MNIST.
+    
+    Cada imagen de dígito es una imagen en escala de grises de 28x28 píxeles, que se aplana
+    en un vector de 784 dimensiones para este modelo. Cada entrada en el vector es un número flotante entre 0 y 1.
 
-    Each handwritten digit is a 28x28 pixel grayscale image, which is flattened
-    into a 784-dimensional vector for the purposes of this model. Each entry in
-    the vector is a floating point number between 0 and 1.
-
-    The goal is to sort each digit into one of 10 classes (number 0 through 9).
-
-    (See RegressionModel for more information about the APIs of different
-    methods here. We recommend that you implement the RegressionModel before
-    working on this part of the project.)
+    El objetivo es clasificar cada dígito en una de las 10 clases (números del 0 al 9).
     """
     def __init__(self):
-        # Initialize your model parameters here
         super().__init__()
-        input_size = 28 * 28
-        output_size = 10
-        "*** YOUR CODE HERE ***"
+        input_size = 28 * 28  # 784 píxeles
+        hidden_size = 200  # Tamaño de la capa oculta
+        output_size = 10  # 10 clases de dígitos
 
-
-
+        # Primera capa lineal: entrada a capa oculta
+        self.linear1 = Linear(input_size, hidden_size)
+        
+        # Segunda capa lineal: capa oculta a salida
+        self.linear2 = Linear(hidden_size, output_size)
 
     def run(self, x):
         """
-        Runs the model for a batch of examples.
+        Ejecuta el modelo para un lote de ejemplos.
 
-        Your model should predict a node with shape (batch_size x 10),
-        containing scores. Higher scores correspond to greater probability of
-        the image belonging to a particular class.
+        El modelo debe predecir un tensor con forma (batch_size x 10),
+        que contenga puntuaciones. Puntuaciones más altas corresponden
+        a una mayor probabilidad de que la imagen pertenezca a una clase en particular.
 
-        Inputs:
-            x: a tensor with shape (batch_size x 784)
-        Output:
-            A node with shape (batch_size x 10) containing predicted scores
-                (also called logits)
+        Entradas:
+            x: un tensor con forma (batch_size x 784)
+        Salida:
+            Un tensor con forma (batch_size x 10) que contiene los logits predichos.
         """
-        """ YOUR CODE HERE """
-
- 
+        # Primera capa con activación ReLU
+        hidden = relu(self.linear1(x))
+        
+        # Segunda capa sin activación (devuelve logits)
+        return self.linear2(hidden)
 
     def get_loss(self, x, y):
         """
-        Computes the loss for a batch of examples.
-
-        The correct labels `y` are represented as a tensor with shape
-        (batch_size x 10). Each row is a one-hot vector encoding the correct
-        digit class (0-9).
-
-        Inputs:
-            x: a node with shape (batch_size x 784)
-            y: a node with shape (batch_size x 10)
-        Returns: a loss tensor
-        """
-        """ YOUR CODE HERE """
-
-    
+        Calcula la pérdida para un lote de ejemplos.
         
+        Las etiquetas correctas `y` están representadas como un tensor con forma
+        (batch_size x 10). Cada fila es un vector one-hot codificando la clase correcta
+        del dígito (0-9).
 
+        Entradas:
+            x: un tensor con forma (batch_size x 784)
+            y: un tensor con forma (batch_size x 10)
+        Retorno:
+            Un tensor con la pérdida calculada.
+        """
+        # Calcula la pérdida de entropía cruzada entre las predicciones y las etiquetas reales
+        predictions = self.run(x)
+        return cross_entropy(predictions, y)
+        
     def train(self, dataset):
         """
-        Trains the model.
+        Entrena el modelo utilizando un conjunto de datos.
         """
-        """ YOUR CODE HERE """
+        # Hiperparámetros
+        batch_size = 100
+        learning_rate = 0.5
+        optimizer = optim.SGD(self.parameters(), lr=learning_rate)
 
-        
-def Convolve(input: tensor, weight: tensor):
-    """
-    Acts as a convolution layer by applying a 2d convolution with the given inputs and weights.
-    DO NOT import any pytorch methods to directly do this, the convolution must be done with only the functions
-    already imported.
+        # Número total de ejemplos de entrenamiento
+        total_examples = len(dataset.x)
 
-    There are multiple ways to complete this function. One possible solution would be to use 'tensordot'.
-    If you would like to index a tensor, you can do it as such:
+        # Bucle de entrenamiento
+        for epoch in range(10):  # Número típico de épocas
+            # Itera sobre el conjunto de datos en lotes
+            for start in range(0, total_examples, batch_size):
+                # Listas para almacenar el lote actual
+                x_batch = []
+                y_batch = []
 
-    tensor[y:y+height, x:x+width]
+                # Recopilar ejemplos del lote actual
+                for i in range(start, min(start + batch_size, total_examples)):
+                    item = dataset[i]
+                    x_batch.append(item['x'])
+                    y_batch.append(item['label'])
 
-    This returns a subtensor who's first element is tensor[y,x] and has height 'height, and width 'width'
-    """
-    input_tensor_dimensions = input.shape
-    weight_dimensions = weight.shape
-    Output_Tensor = tensor(())
-    "*** YOUR CODE HERE ***"
+                # Convertir las listas en tensores
+                x_batch = stack(x_batch)  # Apila los tensores en una nueva dimensión
+                y_batch = stack(y_batch)
 
-    
-    "*** End Code ***"
-    return Output_Tensor
+                # Reiniciar gradientes acumulados
+                optimizer.zero_grad()
 
+                # Calcular la pérdida
+                loss = self.get_loss(x_batch, y_batch)
 
+                # Retropropagación
+                loss.backward()
 
-class DigitConvolutionalModel(Module):
-    """
-    A model for handwritten digit classification using the MNIST dataset.
+                # Actualizar los pesos del modelo
+                optimizer.step()
 
-    This class is a convolutational model which has already been trained on MNIST.
-    if Convolve() has been correctly implemented, this model should be able to achieve a high accuracy
-    on the mnist dataset given the pretrained weights.
-
-    Note that this class looks different from a standard pytorch model since we don't need to train it
-    as it will be run on preset weights.
-    """
-    
-
-    def __init__(self):
-        # Initialize your model parameters here
-        super().__init__()
-        output_size = 10
-
-        self.convolution_weights = Parameter(ones((3, 3)))
-        """ YOUR CODE HERE """
-
-
-
-
-    def run(self, x):
-        return self(x)
- 
-    def forward(self, x):
-        """
-        The convolutional layer is already applied, and the output is flattened for you. You should treat x as
-        a regular 1-dimentional datapoint now, similar to the previous questions.
-        """
-        x = x.reshape(len(x), 28, 28)
-        x = stack(list(map(lambda sample: Convolve(sample, self.convolution_weights), x)))
-        x = x.flatten(start_dim=1)
-        """ YOUR CODE HERE """
-
-
-    def get_loss(self, x, y):
-        """
-        Computes the loss for a batch of examples.
-
-        The correct labels `y` are represented as a tensor with shape
-        (batch_size x 10). Each row is a one-hot vector encoding the correct
-        digit class (0-9).
-
-        Inputs:
-            x: a node with shape (batch_size x 784)
-            y: a node with shape (batch_size x 10)
-        Returns: a loss tensor
-        """
-        """ YOUR CODE HERE """
-
-     
-        
-
-    def train(self, dataset):
-        """
-        Trains the model.
-        """
-        """ YOUR CODE HERE """
-
-
-
-class Attention(Module):
-    def __init__(self, layer_size, block_size):
-        super().__init__()
-        """
-        All the layers you should use are defined here.
-
-        In order to pass the autograder, make sure each linear layer matches up with their corresponding matrix,
-        ie: use self.k_layer to generate the K matrix.
-        """
-        self.k_layer = Linear(layer_size, layer_size)
-        self.q_layer = Linear(layer_size, layer_size)
-        self.v_layer = Linear(layer_size,layer_size)
-
-        #Masking part of attention layer
-        self.register_buffer("mask", torch.tril(torch.ones(block_size, block_size))
-                                     .view(1, 1, block_size, block_size))
-       
-        self.layer_size = layer_size
-
-
-    def forward(self, input):
-        """
-        Applies the attention mechanism to input. All necessary layers have 
-        been defined in __init__()
-
-        In order to apply the causal mask to a given matrix M, you should update
-        it as such:
-    
-        M = M.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))[0]
-
-        For the softmax activation, it should be applied to the last dimension of the input,
-        Take a look at the "dim" argument of torch.nn.functional.softmax to figure out how to do this.
-        """
-        B, T, C = input.size()
-
-        """YOUR CODE HERE"""
-
-     
+            # Comprobar la precisión de validación
+            validation_accuracy = dataset.get_validation_accuracy()
+            
+            # Detener el entrenamiento si la precisión es lo suficientemente alta
+            if validation_accuracy > 0.97:
+                break
